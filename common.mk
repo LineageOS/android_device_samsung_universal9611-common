Roynas-Android-Playground/device_samsung_universal9611-common_old

universal9611: Conditionally enable FOD components

common.mk
@@ -46,9 +46,14 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
 # Biometric Fingerprint
+ifeq ($(TARGET_HAVE_FOD),true)
 PRODUCT_PACKAGES += \
      android.hardware.biometrics.fingerprint@2.3-service-samsung.universal9611 \
      SamsungUdfpsHandler.universal9611
+else
+PRODUCT_PACKAGES += \
+     android.hardware.biometrics.fingerprint@2.3-service.samsung
+endif
 
 # Bluetooth
 PRODUCT_PACKAGES += \

LineageOS/android_device_samsung_universal9611-common

Change-Id: Ie98e2feb96129de6bea784e3539cad7c38f633bd

common.mk
@@ -46,9 +46,14 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
 # Biometric Fingerprint
+ifeq ($(TARGET_HAS_FOD),true)
 PRODUCT_PACKAGES += \
      android.hardware.biometrics.fingerprint@2.3-service-samsung.universal9611 \
      SamsungUdfpsHandler.universal9611
+else
+PRODUCT_PACKAGES += \
+     android.hardware.biometrics.fingerprint@2.3-service.samsung
+endif
 
 # Bluetooth
 PRODUCT_PACKAGES += \