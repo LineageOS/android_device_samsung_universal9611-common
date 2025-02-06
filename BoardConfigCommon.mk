2251e3dbf795e06b4af78b43285ac62f14ecbdf6
cat658011 <cat658011@gmail.com>
2025-02-06 17:09:47 +0500

    universal9611: Remove macro camera id from m21
    Change-Id: I853fc7926d7fdfd0d58d024b4786610aa15f1a41

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -85,7 +85,7 @@ BOARD_DTB_CFG := $(COMMON_PATH)/configs/kernel/exynos9611.cfg
 ## Camera
 SOONG_CONFIG_NAMESPACES += samsungCameraVars
 SOONG_CONFIG_samsungCameraVars += extra_ids
-ifeq ($(TARGET_DEVICE),f41)
+ifeq ($(TARGET_DEVICE),f41, m21)
 SOONG_CONFIG_samsungCameraVars_extra_ids := 4,20,23,50,52
 else
 SOONG_CONFIG_samsungCameraVars_extra_ids := 4,20,23,50,52,54
